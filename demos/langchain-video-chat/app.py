5d4cc3008081f62fd8f9ced4d9dae5a2c4252379
Derrick Mwiti <mwitiderrick@gmail.com>
2023-10-13 10:15:11 +0300

    Update app.py

--- demos/langchain-video-chat/app.py
+++ demos/langchain-video-chat/app.py
@@ -22,25 +22,6 @@ embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-
 llm = DeepSparse(model=MODEL_PATH)
 model = whisper.load_model("base")
 
-system_template = """Use the following pieces of context to answer the users question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
-The "SOURCES" part should be a reference to the source of the document from which you got your answer.
-Example of your response should be:
-```
-The answer is foo
-SOURCES: xyz
-```
-Begin!
-----------------
-{summaries}"""
-messages = [
-    SystemMessagePromptTemplate.from_template(system_template),
-    HumanMessagePromptTemplate.from_template("{question}"),
-]
-prompt = ChatPromptTemplate.from_messages(messages)
-chain_type_kwargs = {"prompt": prompt}
-
 def transcribe(youtube_url, model):
     youtube = YouTube(youtube_url)
 
